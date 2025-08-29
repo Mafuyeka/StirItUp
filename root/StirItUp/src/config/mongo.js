c26bf1574d2c22ab809c9d01e0c615c915395f7a
Tshepiso Rosa <132429932+Mafuyeka@users.noreply.github.com>
2025-08-29 19:33:17 +0200

    Update mongo.js

--- root/StirItUp/src/config/mongo.js
+++ root/StirItUp/src/config/mongo.js
@@ -1,14 +1,12 @@
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import mongoose from 'mongoose';
+
+dotenv.config({ path: path.join(__dirname, '../../.env') });
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-// Load .env before using process.env
-dotenv.config({ path: path.join(__dirname, '../../.env') });
-
-import mongoose from 'mongoose';
 
 const uri = process.env.MONGO_URI;
 if (!uri) {
