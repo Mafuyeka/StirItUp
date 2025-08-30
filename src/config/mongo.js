ed5f007d7009813e2ab9cd154e0e0b302de7c81a
Tshepiso Rosa <132429932+Mafuyeka@users.noreply.github.com>
2025-08-30 20:38:09 +0200

    Update mongo.js

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,6 +1,20 @@
+// config/mongo.js
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+// Load .env explicitly here
+dotenv.config();
+
 const uri = process.env.MONGO_URI;
-if (!uri) { console.error('Missing MONGO_URI'); process.exit(1); }
+
+if (!uri) {
+  console.error('Missing MONGO_URI');
+  process.exit(1);
+}
+
 mongoose.connect(uri, { dbName: uri.split('/').pop() })
   .then(() => console.log('MongoDB connected'))
-  .catch(err => { console.error('Mongo error', err); process.exit(1); });
+  .catch(err => { 
+    console.error('Mongo error', err); 
+    process.exit(1); 
+  });
