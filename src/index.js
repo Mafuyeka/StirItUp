56260932f29b1eaf48f1903d11db101cc109e670
Tshepiso Rosa <132429932+Mafuyeka@users.noreply.github.com>
2025-08-30 20:27:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import './config/mongo.js';
 
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
